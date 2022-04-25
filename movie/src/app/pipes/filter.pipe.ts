42e85212ba98ef9297f9ac2702b67abb67cc1a32
Tan Vo <lonelyboy4746@gmail.com>
2022-04-25 17:43:23 +0700

    fix css for detail page

--- movie/src/app/pipes/filter.pipe.ts
+++ movie/src/app/pipes/filter.pipe.ts
@@ -5,14 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   transform(items, search: string) {
-    if (items.length !== 0) {
-      return items.filter(
-        (item) =>
-          item &&
-          item.title &&
-          item.title.toLowerCase().indexOf(search.toLowerCase()) > -1
-      );
+    if (!search) {
+      return items;
     }
+
+    try {
+      if (items.length !== 0) {
+        return items.filter(
+          (item) =>
+            item &&
+            item.title &&
+            item.title.toLowerCase().indexOf(search.toLowerCase()) > -1
+        );
+      }
+    } catch (error) {
+      debugger;
+    }
+
     return null;
   }
 }
